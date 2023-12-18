5f1fbb1608272868561d4e2dc4a0853d2e11dc9d
Rayyasa <rayya.a192018@gmail.com>
2023-12-18 14:26:29 +0700

    calendar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,68 +4,9 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <>
-      {/* <footer className="footer p-10 bg-base-200 text-base-content mt-10 border border-t-black border-opacity-20">
-        <aside className="flex flex-col">
-          <button>
-            <Image src={Logo} alt="logo" width={400} />
-          </button>
-          <p className="font-medium">
-            R3_Project Team
-            <br />
-            <a href="https://smkmadinatulquran.sch.id/">SMK Madinatul Qur&apos;an</a>
-          </p>
-        </aside>
-        <nav>
-          <header className="footer-title">About Us</header>
-          <a className="link hover:font-semibold  hover:translate-x-2 no-underline transition-all duration-500 ease-in-out">
-            User_ig
-          </a>
-          <a className="link hover:font-semibold  hover:translate-x-2 no-underline transition-all duration-500 ease-in-out">
-            User_ig
-          </a>
-          <a className="link hover:font-semibold  hover:translate-x-2 no-underline transition-all duration-500 ease-in-out">
-            User_ig
-          </a>
-        </nav>
-        <nav>
-          <header className="footer-title">Contact</header>
-          <a className="link hover:font-semibold  hover:translate-x-2 no-underline transition-all duration-500 ease-in-out">
-            12983801289
-          </a>
-          <a className="link hover:font-semibold  hover:translate-x-2 no-underline transition-all duration-500 ease-in-out">
-            102938129083
-          </a>
-          <a className="link hover:font-semibold  hover:translate-x-2 no-underline transition-all duration-500 ease-in-out">
-            1923012983012
-          </a>
-        </nav>
-        <form className="flex flex-col justify-end">
-          <header className="footer-title">Newsletter</header>
-          <fieldset className="form-control w-80">
-            <label className="label">
-              <span className="label-text">Enter your email address</span>
-            </label>
-            <div className="join">
-              <input
-                type="text"
-                placeholder="Enter your email here"
-                className="input input-bordered join-item"
-              />
-              <button className="btn btn-neutral join-item hover:bg-white hover:text-neutral transition-all duration-500 ease-in-out">
-                Send
-              </button>
-            </div>
-          </fieldset>
-        </form>
-      </footer>
-      <footer className="footer text-center justify-center items-center py-4">
-        <p className="font-semibold text-sm">
-          Copyright © 2023 - R3_Project Made with 🧠
-        </p>
-      </footer> */}
-      <footer className="footer footer-center p-10 bg-base-200 text-base-content">
+      <footer className="footer footer-center p-10 bg-slate-200 text-base-content mt-16">
         <aside>
-          {/* <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="inline-block fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg> */}
+      
           <Image src={Logo} alt="logo" width={400} />
           <p className="font-bold">
             R3_Project Team
